cd4c5a657d1fc2362a54ae71bdfa6068e08e0ea2
anacespindola <anacespindola123@gmail.com>
2022-07-05 21:41:18 -0300

    TC01: create ArrowsBGHomepage

---
+++ src/components/headerArrowHomepage/index.jsx
@@ -0,0 +1,17 @@
+export const HomeHeaderDownArrow = () => {
+  return (
+    <div className="custom-shape-divider-top-1657030231">
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+      >
+        <path
+          d="M1200 0L0 0 598.97 114.72 1200 0z"
+          className="shape-fill"
+        ></path>
+      </svg>
+    </div>
+  );
+};
