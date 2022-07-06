859c38a5f54c703ec6314c7165845f97a1c5dac0
anacespindola <anacespindola123@gmail.com>
2022-07-05 21:41:47 -0300

    TC01: create HeaderHome

---
+++ src/components/headerHome/index.jsx
@@ -0,0 +1,23 @@
+import { TopHeaderHome } from "../topHeaderHome";
+import { HomeHeaderDownArrow } from "../headerArrowHomepage";
+import { HomeHeaderArrowContainer } from "../headerArrowHomepage/style";
+import { HeaderMidArrow } from "../headerMidArrows";
+
+export const HeaderHome = () => {
+  return (
+    <>
+      <TopHeaderHome />
+      <HomeHeaderArrowContainer BgColor="#1a02a1f2" transitionColor="#365DA7">
+        <HomeHeaderDownArrow />
+      </HomeHeaderArrowContainer>
+      <HeaderMidArrow />
+      <HomeHeaderArrowContainer
+        BgColor="#ffffff"
+        transitionColor="#365DA7"
+        shape="rotate(180deg);"
+      >
+        <HomeHeaderDownArrow />
+      </HomeHeaderArrowContainer>
+    </>
+  );
+};
