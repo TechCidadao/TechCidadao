d6e7b417930827ff33881981b1030ec4c71aaea5
anacespindola <anacespindola123@gmail.com>
2022-07-05 21:42:25 -0300

    TC01: create route HomePage

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,13 @@
+import { HeaderHome } from "../../components/headerHome";
+import { Footer } from "../../components/footerHome";
+import { MainHome } from "../../components/mainHomepage";
+
+export const HomePage = () => {
+  return (
+    <>
+      <HeaderHome />
+      <MainHome />
+      <Footer />
+    </>
+  );
+};
