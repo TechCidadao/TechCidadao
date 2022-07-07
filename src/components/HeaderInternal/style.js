6b550cc0606f2e94ace70e4a8a524c1514319e76
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-07-07 17:03:07 -0300

    HotFix: Add border in button header

--- src/components/HeaderInternal/style.js
+++ src/components/HeaderInternal/style.js
@@ -128,4 +128,10 @@ export const ButtonEnd = styled.button`
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin-left: 5px;
   border: none;
+
+  &:hover,
+  &:active,
+  &focus {
+    border: 4px solid var(--black);
+  }
 `;
