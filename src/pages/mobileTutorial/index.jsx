209c5b0c9afa093160a0ec50eb5fba2b25fcfc5c
guilherme soares <guilherme.dev36@gmail.com>
2022-07-11 13:50:34 -0300

    feat: click acess

--- src/pages/mobileTutorial/index.jsx
+++ src/pages/mobileTutorial/index.jsx
@@ -10,12 +10,27 @@ import {
 import { CardsTutorial } from "components/cardsTutorial";
 import { Button } from "components/button";
 
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 import IconSocialMedia from "../../assets/iconSocialMedia.png";
 import IconPeople from "../../assets/iconMySpace.png";
 import ArrowTitle from "../../assets/ArrowTitle.png";
 import IconCell from "../../assets/iconCell.png";
 
 export const MobileTutorial = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.addEventListener("keydown", detectKeyDown, true);
+  }, []);
+
+  const detectKeyDown = (e) => {
+    if (e.key === "Escape") {
+      navigate("/dashboard");
+    }
+  };
+
   return (
     <Container>
       <ContentContainer>
